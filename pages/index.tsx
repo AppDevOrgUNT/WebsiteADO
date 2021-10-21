b8c08ccf3d01e8261bb37a2d39681461a096fe21
mcp31 <mcplana31@gmail.com>
2021-10-21 15:06:47 -0500

    Added comment describing what needs to be done

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
-// 
-
 const Home: NextPage = () => {
   return (
     <>
@@ -37,6 +35,7 @@ const Home: NextPage = () => {
             <p className={styles.clubDescription}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
+            {/* Maybe add a hover functionality later */}
             <button className={styles.button} type="button">Join our Discord</button>
           </div>
         </div>
