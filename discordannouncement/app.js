79ee3abe2fb95c23c7db4a6658da23a963995424
GabrielCossio <gabriel_cossio@yahoo.com>
2021-11-03 15:41:02 -0500

    logs all discord messages

---
+++ discordannouncement/app.js
@@ -0,0 +1,55 @@
+const WebSocket = require('ws')
+
+const ws = new WebSocket('wss://gateway.discord.gg/?v=6&encoding=json')
+let inerval = 0;
+
+
+// get token by typing in any chat in the discord server and hitting ctr + shift + i
+
+token = ''
+payload = {
+    op:2,
+    d:{
+        token: token,
+        intents: 513,
+        properties:{
+            $os: 'linux',
+            $browser: 'chrome',
+            $device: 'chrome'
+        }
+
+    }
+}
+
+ws.on('open', function open() {
+    ws.send(JSON.stringify(payload))
+})
+
+ws.on('message', function incoming(data){
+    let payload = JSON.parse(data)
+    const {t, event, op, d} = payload;
+
+
+    //switch can handle more error codes 
+    //error info available on discord gateway documentation
+    switch (op){
+        case 10:
+            const{heartbeat_interval} = d;
+            interval = heartbeat(heartbeat_interval);
+            break;
+    }
+
+    switch (t){
+        case 'MESSAGE_CREATE':
+            let author = d.author.username;
+            let content = d.content
+            console.log('${author}: ${content}')
+    }
+})
+
+//so connection does not time out
+const heartbeat = (ms) => {
+    return setInterval(()=> {
+        ws.send(JSON.stringify({op: 1, d: null}))
+    }, ms)
+}
