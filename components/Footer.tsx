cf039bb4e7b64e234a895928c5740327edaef945
Nicholas Tindle <nicktindle@outlook.com>
2021-11-04 00:00:36 +0000

    in progress

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,33 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const VercelFooter = ()=>{
+  return (
+    <a
+    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Powered by{" "}
+    <span className={styles.logo}>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    </span>
+  </a>
+  );
+};
+
+const NetlifyFooter =()=>{
+  return (
+    <a href="https://www.netlify.com">
+  <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
+</a>
+  );
+};
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{" "}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
+     <div children={platform=="vercel":<VercelFooter/>?<NetflifyFooter/>}>
+       </div
     </footer>
   );
 };
