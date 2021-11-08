76d5f6e044a0e86cb47fb2772132d441135bf65d
mcp31 <mcplana31@gmail.com>
2021-11-08 15:51:35 -0600

    changed overall theme to match the figma

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,15 +15,15 @@ const Home: NextPage = () => {
       </Head>
       <Layout>
         {/* For navigation bar - temp ui; make this sticky*/}
-        <div className={styles.row}>
-          <div className={styles.columnItem}>
+        {/* <div className={styles.row}> */}
+          {/* <div className={styles.columnItem}>
             <ul className={styles.navBar}>
               <li><a href="#welcome">App Dev Org</a></li>
               <li><a href="#whoweare">Who We Are</a></li>
               <li><a href="#projshowcase">Project Showcase</a></li>
               <li><a href="#events">Upcoming Events</a></li>
             </ul>
-          </div>
+          </div> */}
 
           {/* Welcome Area */}
           <div className={styles.sectionContainer} id="welcome">
@@ -31,16 +31,20 @@ const Home: NextPage = () => {
               We are UNT&apos;s
             </h2>
             <h1 className={styles.clubName}>
-              App Development Org
+              Application Development Organization
             </h1>
             <p className={styles.clubDescription}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             {/* Maybe add a hover functionality later */}
-            <button className={styles.button} type="button" onClick={(e) => {
+            <button className={styles.primaryButton} type="button" onClick={(e) => {
               e.preventDefault();
               window.location.href = 'https://discord.gg/eg3KqNsXEc';
-            }}>Join our Discord</button>
+            }}>JOIN OUR DISCORD</button>
+            <button className={styles.secondaryButton} type="button" onClick={(e) => {
+              // e.preventDefault();
+              // window.location.href = 'https://discord.gg/eg3KqNsXEc';
+            }}>LEARN MORE</button>
           </div>
           
 
@@ -52,11 +56,11 @@ const Home: NextPage = () => {
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
-            <div className={styles.pictureDecor}>
-              {/* Have a horizontal scrolling bar here - may have to leave it for later */}
+            {/* <div className={styles.pictureDecor}>
+              {/* Have a horizontal scrolling bar here - may have to leave it for later }
               <MemberScroll/>
               
-            </div>
+            </div> */}
           </div>
 
           {/* Project Showcase */}
@@ -97,7 +101,7 @@ const Home: NextPage = () => {
             </p>
           </div>
 
-        </div> {/* Row Div - may rename later */}
+        {/* </div> Row Div - may rename later */}
       </Layout>
     </>
   );
